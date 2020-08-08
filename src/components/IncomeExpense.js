bf656fb0d03532b06f12f8ce8494e4a555eeefe6
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-08 21:57:45 +0530

    income expense component completed ..git add .

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const IncomeExpense = () => {
-  return <div className="tracker__inc__exp">Income Expense goes here</div>;
+  return (
+    <div className="tracker__inc__exp">
+      <div className="tracker__income">
+        <h4>Income</h4>
+        <i class="fas fa-rupee-sign"></i>
+        <span> 400</span>
+      </div>
+      <div className="tracker__expense">
+        <h4>Expense</h4>
+        <i class="fas fa-rupee-sign"></i>
+        <span> 400</span>
+      </div>
+    </div>
+  );
 };
 
 export default IncomeExpense;
