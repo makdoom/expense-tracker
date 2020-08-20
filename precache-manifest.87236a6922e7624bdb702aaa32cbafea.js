c6569d1348d849dc6c990cbc1877fb0e90f78cea
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-20 10:26:17 +0530

    Updates

---
+++ precache-manifest.87236a6922e7624bdb702aaa32cbafea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bd6904985e1a7f803857cb35612f18",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "71a168a4ebb926db6e06",
+    "url": "/expense-tracker/static/css/main.8e3a26fa.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a168a4ebb926db6e06",
+    "url": "/expense-tracker/static/js/main.734d9246.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
